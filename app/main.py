0c17413d6107faf83bd013dfe900fefaa79ebffe
Guillaume Saurin <guillaume.saurin@slagsoa-consulting.net>
2025-05-12 17:01:55 +0200

    setup/env-and-readme : - App python setup focntionnel - Amélioration README - Ajout env.example

--- app/main.py
+++ app/main.py
@@ -1,15 +1,10 @@
-from flask import Flask, request, jsonify
-import pandas as pd
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/analyse", methods=["POST"])
-def analyse_csv():
-    file = request.files["file"]
-    df = pd.read_csv(file)
-    # Exemple d'analyse simple
-    summary = df.describe().to_dict()
-    return jsonify({"summary": summary})
+@app.route("/", methods=["GET"])
+def home():
+    return "Zukii Analyse Service is running!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
